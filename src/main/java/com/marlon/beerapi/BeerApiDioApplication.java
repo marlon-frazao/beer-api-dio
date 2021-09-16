9ff1545be9368363456d1ae1e4d326dd174aa947
marlon-frazao <frazao.marlon@gmail.com>
2021-09-16 13:26:58 -0300

    Project created

---
+++ src/main/java/com/marlon/beerapi/BeerApiDioApplication.java
@@ -0,0 +1,13 @@
+package com.marlon.beerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerApiDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerApiDioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marlon/beerapi/BeerApiDioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marlon.beerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerApiDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
