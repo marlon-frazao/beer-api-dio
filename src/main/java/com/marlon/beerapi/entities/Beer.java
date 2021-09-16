d6e8d5faa0eefe45c27bcb4e6fca36e51a6cddac
marlon-frazao <frazao.marlon@gmail.com>
2021-09-16 13:46:24 -0300

    Beer and BeerType created

---
+++ src/main/java/com/marlon/beerapi/entities/Beer.java
@@ -0,0 +1,110 @@
+package com.marlon.beerapi.entities;
+
+import java.io.Serializable;
+
+import com.marlon.beerapi.enums.BeerType;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Beer implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false, unique = true)
+	private String name;
+	
+	@Column(nullable = false)
+	private String brand;
+	
+	@Column(nullable = false)
+	private int quantity;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private BeerType type;
+	
+	public Beer() {}
+
+	public Beer(Long id, String name, String brand, int quantity, BeerType type) {
+		this.id = id;
+		this.name = name;
+		this.brand = brand;
+		this.quantity = quantity;
+		this.type = type;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public BeerType getType() {
+		return type;
+	}
+
+	public void setType(BeerType type) {
+		this.type = type;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Beer other = (Beer) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+}
---
+++ src/main/java/com/marlon/beerapi/enums/BeerType.java
@@ -0,0 +1,22 @@
+package com.marlon.beerapi.enums;
+
+public enum BeerType {
+
+	LAGER("Lager"), 
+	MALZBIER("Malzbier"),
+	WITBIER("Witbier"),
+    WEISS("Weiss"),
+    ALE("Ale"),
+    IPA("IPA"),
+    STOUT("Stout");
+
+    private final String description;
+
+	private BeerType(String description) {
+		this.description = description;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+}
